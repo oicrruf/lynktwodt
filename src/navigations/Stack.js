0bad48ea0964ea1caab48aa766ea339015a3cbd3
oicrruf <vm.reyesal@gmail.com>
2020-12-04 15:26:35 -0600

    Añadiendo background y logo en barra de navegación

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -1,14 +1,26 @@
 import * as React from 'react';
+import {Button} from 'react-native-elements';
+import {Image} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {createStackNavigator} from '@react-navigation/stack';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
+import {cleanLogin} from '../function/Realmio';
 import Login from '../screens/Login';
 import Journey from '../screens/Journey';
 import Delivery from '../screens/Delivery';
 import Detail from '../screens/Detail';
 
+const Logo = () => {
+  return (
+    <Image
+      style={{alignSelf: 'center'}}
+      source={require('../assets/images/header.png')}
+    />
+  );
+};
 const Stack = createStackNavigator();
 
 const Navigation = () => {
@@ -20,6 +32,22 @@ const Navigation = () => {
         headerStyle: {
           elevation: 0,
         },
+        headerTitle: (props) => <Logo {...props} />,
+        headerRight: () => (
+          <Button
+            icon={<Icon name="sign-out" size={20} color="#FFFFFF" />}
+            onPress={() => {
+              console.log('cerrar sesion');
+            }}
+            color="#1F57E5"
+            type="clear"
+            buttonStyle={{
+              width: hp(6),
+              height: hp(6),
+              borderRadius: hp(12),
+            }}
+          />
+        ),
       }}>
       <Stack.Screen
         name="Login"
@@ -32,8 +60,29 @@ const Navigation = () => {
         name="Journey"
         component={Journey}
         options={{
-          headerTransparent: true,
+          headerTransparent: false,
+          headerTintColor: '#FFFFFF',
+          headerStyle: {
+            height: hp(9),
+            backgroundColor: '#FFFFFF',
+            elevation: 0,
+          },
           title: false,
+          headerRight: () => (
+            <Button
+              icon={<Icon name="sign-out" size={20} color="#1F57E5" />}
+              onPress={() => {
+                console.log('cerrar sesion');
+              }}
+              color="#1F57E5"
+              type="clear"
+              buttonStyle={{
+                width: hp(6),
+                height: hp(6),
+                borderRadius: hp(12),
+              }}
+            />
+          ),
         }}
       />
       <Stack.Screen
--- src/screens/Journey.js
+++ src/screens/Journey.js
@@ -1,77 +1,76 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button, Input} from 'react-native-elements';
-import {StyleSheet, View, Image, Text} from 'react-native';
+import {StyleSheet, View, Image, Text, ImageBackground} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
-import {axiosRequest} from "../function/Request";
-import {checkSession,readData} from "../function/Realmio";
-
+import {axiosRequest} from '../function/Request';
+import {checkSession, readData} from '../function/Realmio';
 
 const Journey = ({navigation}) => {
-
-  /// Verifico la session del usuario 
+  /// Verifico la session del usuario
   useEffect(() => {
     checkSession(navigation);
   });
   // info AXIOS
   const DataSave = readData();
 
-  const CompanyInfo = {id:DataSave.id, access_token:DataSave.access_token};
+  const CompanyInfo = {id: DataSave.id, access_token: DataSave.access_token};
   console.log(CompanyInfo);
-        
+
   /// Get info placas y name users
-  axiosRequest('info','get',CompanyInfo).then((resultInfoCompany) => {  
-  
-    if(resultInfoCompany.data){
-      const dataPlacas = resultInfoCompany.data[0];
-        navigation.push("Journey");
-    }
-
-  }).catch(function (error) {
-    console.log(error);
-  }); 
-
-
+  axiosRequest('info', 'get', CompanyInfo)
+    .then((resultInfoCompany) => {
+      if (resultInfoCompany.data) {
+        const dataPlacas = resultInfoCompany.data[0];
+        navigation.push('Journey');
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
   const [plate, setPlate] = useState({
     plate: 'P469113',
   });
 
-  
   return (
     <>
       <View style={styles.containter}>
-        <View style={styles.header}>
-          <View style={styles.logoContainer}>
-            <Image
-              style={styles.logo}
-              source={require('../assets/images/logo.png')}
-            />
+        <View style={styles.body}>
+          <View style={styles.header}>
+            <View style={styles.logoContainer}>
+              <Image
+                style={styles.logo}
+                source={require('../assets/images/logo.png')}
+              />
+            </View>
           </View>
-        </View>
-        <View style={styles.inputs}>
-          <Text>Bienvenido Ricardo Polanco</Text>
-          <Picker
-            selectedValue={plate.plate}
-            style={{height: hp(5), width: wp(90)}}
-            onValueChange={(itemValue, itemIndex) =>
-              setPlate({language: itemValue})
-            }>
-            <Picker.Item label="P469113" value="1" />
-            <Picker.Item label="P886468" value="2" />
-          </Picker>
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            buttonStyle={styles.button}
-            title="Iniciar viaje"
-            onPress={() => {
-              navigation.navigate('Delivery');
-            }}
-          />
+          <View style={styles.inputs}>
+            <Text>Bienvenido Ricardo Polanco</Text>
+            <Picker
+              selectedValue={plate.plate}
+              style={{height: hp(5), width: wp(90)}}
+              onValueChange={(itemValue, itemIndex) =>
+                setPlate({language: itemValue})
+              }>
+              <Picker.Item label="P469113" value="1" />
+              <Picker.Item label="P886468" value="2" />
+            </Picker>
+          </View>
+          <ImageBackground
+            source={require('../assets/images/background.png')}
+            style={styles.buttonContainer}>
+            <Button
+              buttonStyle={styles.button}
+              title="Iniciar viaje"
+              onPress={() => {
+                navigation.navigate('Delivery');
+              }}
+            />
+          </ImageBackground>
         </View>
       </View>
     </>
@@ -83,6 +82,11 @@ const styles = StyleSheet.create({
     height: hp(100),
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  body: {
+    marginTop: -hp(17),
+    paddingTop: hp(6),
+    width: wp(100),
   },
   header: {
     height: hp(100) / 3,
@@ -113,9 +117,10 @@ const styles = StyleSheet.create({
   buttonContainer: {
     height: hp(100) / 3,
     width: wp(100),
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#1F57E5',
+    resizeMode: 'cover',
+    justifyContent: 'center',
   },
   button: {
     width: wp(90),
